625d9b0bc754f6a51b67a79fb4b522f2eb55d7c7
Yunus Bayraktar <ybayraktar@adobe.com>
2024-06-11 16:13:33 +0200

    day4 assignment changes.

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,6 +1,10 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
+import { createOptimizedPicture, fetchPlaceholders } from '../../scripts/aem.js';
 
-export default function decorate(block) {
+export default async function decorate(block) {
+  // fetch placeholders from the 'en' folder
+  const placeholders = await fetchPlaceholders('default');
+  const { clickHereForMore } = placeholders;
+
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
@@ -10,6 +14,13 @@ export default function decorate(block) {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
     });
+    const openCardBtn = document.createElement('a');
+    openCardBtn.classList.add('button');
+    openCardBtn.href = '/';
+    openCardBtn.textContent = clickHereForMore;
+    openCardBtn.title = clickHereForMore;
+
+    li.append(openCardBtn);
     ul.append(li);
   });
   ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
