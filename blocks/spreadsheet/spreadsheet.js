550729db15fb57b88fcf6c4b2e1df9652d52b341
Yunus Bayraktar <ybayraktar@adobe.com>
2024-06-11 22:33:20 +0200

    day5 assignment changes.

--- blocks/spreadsheet/spreadsheet.js
+++ blocks/spreadsheet/spreadsheet.js
@@ -12,63 +12,69 @@ function buildCell(rowIndex) {
 
 /**
  * Loads a spreadsheet.
+ * @param {Element} block The block element
  * @param {string} path The path to the spreadsheet
  * @returns {HTMLElement} The root element of the spreadsheet
  */
 export async function loadSpreadsheet(block, path) {
+  window.spreadsheet = window.spreadsheet || {};
   if (path) {
-    fetch(path)
-      .then((resp) => {
-        if (resp.ok) {
-          return resp.json();
-        }
-        return {};
-      })
-      .then((json) => {
-        if (!json || !json.data || json.data.length === 0) {
-          return null;
-        }
+    window.spreadsheet = new Promise((resolve) => {
+      fetch(path)
+        .then((resp) => {
+          if (resp.ok) {
+            return resp.json();
+          }
+          return {};
+        })
+        .then((json) => {
+          if (!json || !json.data || json.data.length === 0) {
+            window.spreadsheet = null;
+            resolve(window.spreadsheet);
+          }
 
-        const colNames = {};
-        const firstRow = json.data[0];
-        Object.keys(firstRow).forEach((colName) => {
-          colNames[colName] = colName;
-        });
-
-        const table = document.createElement('table');
-        const thead = document.createElement('thead');
-        const tbody = document.createElement('tbody');
-
-        const header = !block.classList.contains('no-header');
-        if (header) {
-          table.append(thead);
-
-          const rowElement = document.createElement('tr');
-          Object.keys(colNames).forEach((colName) => {
-            const cell = buildCell(0);
-            cell.innerHTML = colName;
-            rowElement.append(cell);
+          const colNames = {};
+          const firstRow = json.data[0];
+          Object.keys(firstRow).forEach((colName) => {
+            colNames[colName] = colName;
           });
-          thead.append(rowElement);
-        }
-        table.append(tbody);
 
-        json.data
-          .forEach((row, i) => {
+          const table = document.createElement('table');
+          const thead = document.createElement('thead');
+          const tbody = document.createElement('tbody');
+          const header = !block.classList.contains('no-header');
+          if (header) {
+            table.append(thead);
             const rowElement = document.createElement('tr');
             Object.keys(colNames).forEach((colName) => {
-              const cell = buildCell(i + 1);
-              cell.innerHTML = row[colName];
+              const cell = buildCell(0);
+              cell.innerHTML = colName;
               rowElement.append(cell);
             });
-            tbody.append(rowElement);
-          });
+            thead.append(rowElement);
+          }
+          table.append(tbody);
+          json.data
+            .forEach((row, i) => {
+              const rowElement = document.createElement('tr');
+              Object.keys(colNames).forEach((colName) => {
+                const cell = buildCell(i + 1);
+                cell.innerHTML = row[colName];
+                rowElement.append(cell);
+              });
+              tbody.append(rowElement);
+            });
 
-        return table;
-      })
-      .catch(() => null);
+          window.spreadsheet = table;
+          resolve(window.spreadsheet);
+        })
+        .catch(() => {
+          window.spreadsheet = null;
+          resolve(window.spreadsheet);
+        });
+    });
   }
-  return null;
+  return window.spreadsheet;
 }
 
 export default async function decorate(block) {
